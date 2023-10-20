548b298090f783092889b0ae997a4a335e4ce023
Nithik Yekollu <nithik@berkeley.edu>
2023-10-19 20:56:14 -0700

    implemented gradio

--- app.py
+++ app.py
@@ -1,68 +1,53 @@
+from anyio import sleep
 import gradio as gr
-import openai
 
-import json
-import yaml
-import re
-import os
-from dotenv import load_dotenv
+#Function for first part to generate the adaptor
+def generate_adapter(ID, URL_List, Domain):
+#Add your function here. Make sure that you return a file that is the adaptor
+    for i in range(10):
+        print(1)
+    #This is where you return the file
+    return 
 
-from utils import OpenAIClient
-from query_gen import 
+#Function to generate inference responses in a chatbot format
+def inference_response_chatbot(input, history):
+#Have the inference with the prompt input return text outpjut appropriately
+    return 
 
-load_dotenv()
-
-OPENAI_KEY = os.environ.get('OPENAI_KEY')
-
-def is_swagger_content(content):
-    try:
-        # Attempt to parse the content as JSON
-        json.loads(content)
-        return True
-    except json.JSONDecodeError:
-        try:
-            # Attempt to parse the content as YAML
-            yaml.safe_load(content)
-            return True
-        except yaml.YAMLError:
-            return False
-
-def is_swagger_file(file):
-    if re.search(r"\.(json|yaml|yml)$", file.name, re.IGNORECASE):
-        with open(file.name, 'rb') as f:
-            content = f.read().decode('utf-8')
-            return is_swagger_content(content)
-    return False
-
-
-    
-
-def execute(inputs):
-    """
-    Input:
-    inputs -> id, url, domain
-    """
-    
-    progress=gr.Progress(track_tqdm=True)
-
-    inst_api_pair = api_to_inst_api_pair(file)
+#Function to generate inference responses in a single text format
+def inference_response_chatbot(input):
+#Have the inference with the prompt input return text outpjut appropriately
+    return 
 
 
 
 def main():
-    # Define Gradio interface with File input
-    iface = gr.Interface(
-        fn=execute,
-        inputs=[
-            gr.inputs.Textbox(label="ID"),
-            gr.inputs.Textbox(label="URL"),
-            gr.inputs.Textbox(label="Domain")
-        ],
-        outputs=gr.outputs.Textbox(label="Result"),
+    #This is the interface used for generating the adaptor
+    finetuning = gr.Interface(
+        fn=generate_adapter,
+        inputs=["text", "text", "text"],
+        outputs=gr.File(label="Adapter", elem_id="result-adapter"),
+        allow_flagging="never",
+
     )
 
-    # Launch the Gradio interface
-    iface.launch()
+    #interface for playground. Have included two options, a single text field for input and for output or a more chat gpt like plaground
+    playground_chatbot = gr.ChatInterface(inference_response_chatbot)
+
+    playground_single_text = gr.Interface(
+        fn=inference_response_chatbot,
+        inputs=["text"],
+        outputs=["text"],
+        allow_flagging="never",
+
+    )
+
+    
+    
+    demo = gr.TabbedInterface([finetuning, playground_chatbot], ["Finetuning", "Playground"])
+
+    demo.launch()
 
 if __name__ == "__main__":
     main()
+
